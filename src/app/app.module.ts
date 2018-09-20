54e12671a8eb41db6bf7d1dec1631ef646df00c5
Fernando Baba <fernando.baba@iress.com.au>
2018-09-20 17:29:12 +1000

    Upgrade library to Angular 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CONFIGURATION_FACTORY, ConfigurationProvider, ConfigurationModule } from './ngx-configuration/public-api';
-
+import { CONFIGURATION_FACTORY, ConfigurationModule, ConfigurationProvider } from './ngx-configuration/public-api';
 
 export function ConfigureFactory() {
   return (config: ConfigurationProvider<any>) => {
-    return config.loadDefault().then(x => config.merge({ settingB: 'Anothervalue' }));
+    return config
+      .loadDefault()
+      .then(x => config.merge({ settingB: 'Anothervalue' }));
   };
 }
 
-
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, ConfigurationModule],
+  providers: [
+    {
+      provide: CONFIGURATION_FACTORY,
+      useFactory: ConfigureFactory,
+      multi: true
+    }
   ],
-  imports: [
-    BrowserModule,
-    EnvironmentConfigurationModule
-  ],
-  providers: [{
-    provide: CONFIGURATION_FACTORY,
-    useFactory: ConfigureFactory,
-    multi: true
-  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
